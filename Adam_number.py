c80e91b7a86fda01131d288fcba782a86ec804d2
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2023-05-19 22:58:46 +0530

    n=int(input()) t=n s=0 c=0 while n!=0:     r=n%10     s=s*10+r     n=n//10 a=s*s b=t*t while a!=0:     z=a%10     c=c*10+z     a=a//10 if b==c:     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,18 +1,18 @@
 n=int(input())
 t=n
 s=0
-z=0
-while(n!=0):
+c=0
+while n!=0:
     r=n%10
     s=s*10+r
     n=n//10
 a=s*s
 b=t*t
-while(a!=0):
-    c=a%10
-    z=z*10+c
+while a!=0:
+    z=a%10
+    c=c*10+z
     a=a//10
-if z==b:
+if b==c:
     print("True")
 else:
     print("False")
