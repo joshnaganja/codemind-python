ce322097f23f3c9671837c488ad029712069c794
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2023-05-21 18:25:14 +0530

    s=0 n=int(input()) t=n while(n):     i=1     fact=1     r=n%10     while(i<=r):         fact=fact*i         i=i+1     s=s+fact     n=n//10 if(s==t):     print(f"The number {t} is a strong number") else:     print(f"The number {t} is not a strong number")

---
+++ Strong_Number.py
@@ -0,0 +1,16 @@
+s=0
+n=int(input())
+t=n
+while(n):
+    i=1
+    fact=1
+    r=n%10
+    while(i<=r):
+        fact=fact*i
+        i=i+1
+    s=s+fact
+    n=n//10
+if(s==t):
+    print(f"The number {t} is a strong number")
+else:
+    print(f"The number {t} is not a strong number")
