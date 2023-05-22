c9c0bf1ece1e9236d357463ff4b484b4393d2637
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2023-05-22 21:21:52 +0530

    n=int(input()) s=str(n) l1=[] for i in s:     if i not in l1:         l1.append(i)     else:         pass if len(s)==len(l1):     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,6 +1,12 @@
-n=list(input())
-a=set(n)
-if len(n)==len(a):
+n=int(input())
+s=str(n)
+l1=[]
+for i in s:
+    if i not in l1:
+        l1.append(i)
+    else:
+        pass
+if len(s)==len(l1):
     print("Unique Number")
 else:
     print("Not Unique Number")
