12f60161ee43473a57280c8e216e5c316388bddf
joshnaganja <119840321+joshnaganja@users.noreply.github.com>
2023-06-01 15:21:29 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] l2=[] l3=[] for i in l:     if i not in l1:         l1.append(i)     else:         l2.append(i) for i in l1:     if i in l2:         pass     else:         l3.append(i) if len(l3)==0:     print("-1") else:     print(max(l3))

---
+++ UNIQUE_MAXIMUM_NUMBER.py
@@ -0,0 +1,19 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+l2=[]
+l3=[]
+for i in l:
+    if i not in l1:
+        l1.append(i)
+    else:
+        l2.append(i)
+for i in l1:
+    if i in l2:
+        pass
+    else:
+        l3.append(i)
+if len(l3)==0:
+    print("-1")
+else:
+    print(max(l3))
